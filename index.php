21230866b42518f9b4b4564fd8acd30764945c5c
Dashnyam <lord_dashka277@yahoo.com>
2020-11-24 22:10:31 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Main template file
+ * 
+ * @package Aquile
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Some wordpress them</title>
+</head>
+<body>
+blass
+    
+</body>
+</html>
